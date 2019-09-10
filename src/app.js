d8aaee4dd027eadb0e0b9b539f8afb3898bf1550
Chris Haining <chrishaining@yahoo.co.uk>
2019-09-10 16:21:12 +0100

    adds dropdown list

--- src/app.js
+++ src/app.js
@@ -4,24 +4,21 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: '#app',
     data: {
-      countries: [
-        {name: ""
-        // flag: null
-      }
-      ]
+      countries: []
     },
-    // mounted() {
-    //   this.
-    // },
+    mounted() {
+      this.fetchCountries()
+    },
 
     methods: {
       fetchCountries: function() {
         fetch('https://restcountries.eu/rest/v2/all')
         .then(response => response.json())
-        .then(data => this.countries = data.message)
+        .then(data => this.countries = data)
         .catch(console.error);
 
       }
+
 
     }
   })
